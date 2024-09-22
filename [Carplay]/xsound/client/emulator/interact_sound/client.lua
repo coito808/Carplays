221791bd219fed777d9d7dded4ead3ecad17a803
coito808 <131509783+coito808@users.noreply.github.com>
2024-09-22 07:02:14 -0500

    Add files via upload

---
+++ [Carplay]/xsound/client/emulator/interact_sound/client.lua
@@ -0,0 +1,29 @@
+if config.interact_sound_enable then
+
+    RegisterNetEvent('InteractSound_CL:PlayOnOne')
+    AddEventHandler('InteractSound_CL:PlayOnOne', function(soundFile, soundVolume)
+        PlayUrl("./sounds/" .. soundFile, "./sounds/" .. soundFile .. "." .. config.interact_sound_file, soundVolume)
+    end)
+
+    RegisterNetEvent('InteractSound_CL:PlayOnAll')
+    AddEventHandler('InteractSound_CL:PlayOnAll', function(soundFile, soundVolume)
+        PlayUrl("./sounds/" .. soundFile, "./sounds/" .. soundFile .. "." .. config.interact_sound_file, soundVolume)
+    end)
+
+    RegisterNetEvent('InteractSound_CL:PlayWithinDistance')
+    AddEventHandler('InteractSound_CL:PlayWithinDistance', function(playerNetId, maxDistance, soundFile, soundVolume)
+        if GetPlayerFromServerId(playerNetId) ~= -1 then
+            local eCoords = GetEntityCoords(GetPlayerPed(GetPlayerFromServerId(playerNetId)))
+            PlayUrlPos("./sounds/" .. soundFile, "./sounds/" .. soundFile .. "." .. config.interact_sound_file, soundVolume, eCoords)
+            Distance("./sounds/" .. soundFile, maxDistance)
+        end
+    end)
+
+end
+
+-- Updated Destroy Function:
+if soundId then
+    exports['xsound']:Destroy(soundId)
+else
+    print('Error: Invalid sound ID')
+end
